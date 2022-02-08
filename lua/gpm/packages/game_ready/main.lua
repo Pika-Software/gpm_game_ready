6098e8ba19cb269c2d5d52870cf90a3e67d1b7f7
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-08 22:05:20 +0400

    1.0.1

--- lua/gpm/packages/game_ready/main.lua
+++ lua/gpm/packages/game_ready/main.lua
@@ -1,7 +1,4 @@
-local table_insert = table.insert
-local unpack = unpack
 local assert = assert
-local ipairs = ipairs
 local type = type
 
 module( "game_ready", package.seeall )
@@ -17,24 +14,52 @@ function isReady()
     return Ready
 end
 
-function wait( func, ... )
-    assert( type( func ) == "function", "bad argument #1 (function expected)")
-    table_insert( waitingFuncs, { func, { ... } } )
+do
+    local unpack = unpack
+    local ipairs = ipairs
+
+    local function runQueue()
+        for num, tbl in ipairs( waitingFuncs ) do
+            table.remove( waitingFuncs, num )
+            tbl[1]( unpack( tbl[2] ) )
+        end
+    end
 end
 
-local function ready()
-    Ready = true
-
-    for num, tbl in ipairs( waitingFuncs ) do
-        tbl[1]( unpack( tbl[2] ) )
+do
+    local table_insert = table.insert
+    function wait( func, ... )
+        if Ready then
+            func( ... )
+            runQueue()
+        else
+            assert( type( func ) == "function", "bad argument #1 (function expected)")
+            table_insert( waitingFuncs, { func, { ... } } )
+        end
     end
+end
+
+function run( func )
+    assert( type( func ) == "function", "bad argument #1 (function expected)")
+    if Ready then
+        return func()
+    else
+        wait( func )
+    end
+end
+
+local function nowReady()
+    Ready = true
+    runQueue()
+
+    timer.Create( "gpm_game_ready", 60, 0, runQueue )
 end
 
 if CLIENT then
     hook.Add("RenderScene", "gpm_game_ready", function()
         hook.Remove("RenderScene", "gpm_game_ready")
-        ready()
+        nowReady()
     end)
 else
-    timer.Simple(0, ready)
+    timer.Simple(0, nowReady)
 end
