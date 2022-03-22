b4c08be3828a56b2bc52280cc7c0c8525bdaad88
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-03-22 14:38:05 +0400

    1.1.0

--- lua/gpm/packages/game_ready/main.lua
+++ lua/gpm/packages/game_ready/main.lua
@@ -1,44 +1,46 @@
-local assert = assert
-local type = type
-
 module( "game_ready", package.seeall )
 
 local Ready = false
-local waitingFuncs = {}
-
-function ready()
-    return Ready
-end
 
 function isReady()
     return Ready
 end
 
-local unpack = unpack
-local ipairs = ipairs
-
-local function runQueue()
-    for num, tbl in ipairs( waitingFuncs ) do
-        table.remove( waitingFuncs, num )
-        tbl[1]( unpack( tbl[2] ) )
-    end
-end
-
 do
-    local table_insert = table.insert
-    function wait( func, ... )
-        if Ready then
-            func( ... )
-            runQueue()
-        else
-            assert( type( func ) == "function", "bad argument #1 (function expected)")
-            table_insert( waitingFuncs, { func, { ... } } )
+
+    local waitingFuncs = {}
+
+    do
+
+        local unpack = unpack
+        local ipairs = ipairs
+
+        function runQueue()
+            for num, tbl in ipairs( waitingFuncs ) do
+                table.remove( waitingFuncs, num )
+                pcall( tbl[1], unpack( tbl[2] ) )
+            end
         end
+
     end
+
+    do
+
+        local table_insert = table.insert
+        function wait( func, ... )
+            if Ready then
+                func( ... )
+                runQueue()
+            else
+                table_insert( waitingFuncs, { func, { ... } } )
+            end
+        end
+
+    end
+
 end
 
 function run( func )
-    assert( type( func ) == "function", "bad argument #1 (function expected)")
     if Ready then
         return func()
     else
@@ -46,7 +48,7 @@ function run( func )
     end
 end
 
-local function nowReady()
+function ready()
     Ready = true
     runQueue()
 
@@ -54,10 +56,44 @@ local function nowReady()
 end
 
 if CLIENT then
-    hook.Add("RenderScene", "gpm_game_ready", function()
-        hook.Remove("RenderScene", "gpm_game_ready")
-        nowReady()
-    end)
+
+    do
+
+        local LocalPlayer = LocalPlayer
+        hook.Add("ShutDown", "Game Ready:PlayerDisconnected", function()
+            hook.Remove("ShutDown", "Game Ready:PlayerDisconnected")
+
+            local ply = LocalPlayer()
+            if IsValid( ply ) then
+                hook.Run( "PlayerDisconnected", ply )
+            end
+        end)
+
+        hook.Add("RenderScene", "Game Ready:PlayerInitialized", function()
+            hook.Remove( "RenderScene", "Game Ready:PlayerInitialized" )
+
+            local ply = LocalPlayer()
+            ply["Initialized"] = true
+            ready()
+
+            hook.Run("PlayerInitialized", ply)
+        end)
+
+    end
+
 else
-    timer.Simple(0, nowReady)
+
+    hook.Add("PlayerInitialSpawn", "Game Ready:PlayerInitialized", function( ply )
+        hook.Add("SetupMove", ply, function( self, ply, mv, cmd )
+            if (self == ply) and not cmd:IsForced() then
+                hook.Remove( "SetupMove", self )
+                self["Initialized"] = true
+
+                hook.Run( "PlayerInitialized", self )
+            end
+        end)
+    end)
+
+    timer.Simple(0, ready)
+
 end
